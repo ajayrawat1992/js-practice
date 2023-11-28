f55d24a79a510f1dc696762117dc9c5200033e3e
ajayrawat1992 <124458902+ajayrawat1992@users.noreply.github.com>
2023-11-28 06:37:10 +0000

    new entries

--- practice/test.js
+++ practice/test.js
@@ -1,1 +1,11 @@
-console.log("ajay")
+
+const details={
+    name:"ajay",
+    surname:"rawat"
+}
+function printname(letarray)
+{
+    console.log(`your name is ${letarray.name} and surname is ${letarray.surname}` );
+}
+
+printname(details)
