9df014b10507f4c69272e2e1d1f62dbf756c7bd4
ajayrawat1992 <124458902+ajayrawat1992@users.noreply.github.com>
2023-11-28 11:08:58 +0000

    for practice

--- practice/test.js
+++ practice/test.js
@@ -1,11 +1,56 @@
+// function calculate(...mynumber)  //rest operator used to collect the values passed  and present in array
+// {
+// console.log(mynumber)
 
-const details={
-    name:"ajay",
-    surname:"rawat"
-}
-function printname(letarray)
+// }
+// calculate(100,200,300)
+
+
+//============================================================
+
+
+//passing objects in function
+// const details={
+//     name:"ajay",
+//     surname:"rawat"
+// }
+// function printname(letarray)
+// {
+//     console.log(`your name is ${letarray.name} and surname is ${letarray.surname}` );
+// }
+
+// printname(details)
+
+//============================================================
+
+// function outer()
+// {
+//     var num1=100
+//     function inner()
+//     {
+//         var num2=200
+//         console.log(num1)
+//     }
+//     inner()
+//     //console.log(num2) // we cant access the value as it is under inner() function scope
+// }
+// outer()
+
+
+
+//============================================================
+
+
+way1(5)
+function  way1(num)
 {
-    console.log(`your name is ${letarray.name} and surname is ${letarray.surname}` );
+    return  num +1
 }
 
-printname(details)
+
+way2(5)   // gives error ==>Cannot access 'way2' before initialization
+const way2=function(num)
+    {
+        return num+3
+    }
+
